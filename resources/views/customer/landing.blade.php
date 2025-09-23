6b0fcc0a02fb25eb916e9102cd6016c312841e14
KenRFH <riezqyken57@gmail.com>
2025-09-23 16:20:13 +0700

    23September

--- resources/views/customer/landing.blade.php
+++ resources/views/customer/landing.blade.php
@@ -19,6 +19,7 @@
 
 @section('content')
     {{-- =================== HERO VIDEO =================== --}}
+    {{-- =================== HERO VIDEO =================== --}}
     <section class="relative w-full h-screen overflow-hidden">
         <video autoplay muted loop playsinline class="absolute inset-0 w-full h-full object-cover">
             <source src="{{ asset('assets/customer/vid-profil.mp4') }}" type="video/mp4" loading="lazy">
@@ -27,134 +28,111 @@
 
         <div class="absolute inset-0 bg-black/50"></div>
 
-        <img src="{{ asset('assets/customer/truly2.png') }}" alt="Truly Picnic"
-            class="absolute top-3/4 left-1/2 w-1/2 md:w-1/3 transform -translate-x-1/2 -translate-y-1/2 z-20">
+        <img src="{{ asset('assets/customer/truly6.png') }}" alt="Truly Picnic"
+            class="absolute top-1/2 left-1/2 w-1/2 md:w-1/3 md:top-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20">
 
 
-        {{-- Konten Tengah --}}
-        {{-- <div
-            class="absolute inset-0 flex flex-col mx-10 justify-center items-start z-20 text-[#093FB4] text-3xl md:text-6xl">
-            <h1 class="font-poppins">
-                <span class="block font-semibold">Truly</span>
-                <span class="text-[#ED3500] font-semibold text-8xl">Picnic</span>
-            </h1>
-
-            <p class="text-white font-reguler text-left text-[0.9rem] w-full md:w-1/2">Selamat datang di Taman Rekreasi
-                dengan
-                perpaduan pemandangan alam terindah nomer 1 di Kota Batu. Tempat paling nyaman untuk keluarga melihat
-                bunga-bunga indah dengan suasana dingin di lembah Gunung Panderman</p>
-
-            {{-- Tombol --}}
-        <a href="/"
-            class="mt-6 bg-[#093FB4] hover:bg-[#6ECCFF] text-white font-semibold hover:text-[#1c0a66] px-6 py-3 rounded-lg shadow-xl transition duration-400 text-lg">
-            Pesan Tiket
-        </a>
+        <!-- Wave di bawah hero -->
+        <div class="absolute bottom-0 left-0 w-full overflow-hidden leading-none">
+            <svg class="relative block w-full h-[100px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 100"
+                preserveAspectRatio="none">
+                <path fill="#FFFCFB" d="M0,30 C360,90 1080,-30 1440,60 L1440,100 L0,100Z">
+                </path>
+            </svg>
         </div>
     </section>
 
     {{-- =================== TIKET TERBAIK =================== --}}
-    <section class="bg-[#bee5f9] py-10 min-h-[700px] text-center pr-10 relative">
-        <h1 class="text-4xl md:text-7xl mx-10 font-semibold text-[#2a1a71] font-poppins">
-            Pilihan <span class="text-amber-500">Tiket</span>
+    <section class="bg-[#FFFCFB] py-10 min-h-[700px] text-center pr-10 relative rounded-b-9xl">
+        <h1 class="text-4xl md:text-7xl mx-10 font-semibold text-[#093FB4] font-poppins">
+            Pilihan <span class="text-[#ED3500]">Tiket</span>
         </h1>
     </section>
 
+
     <!-- Wave -->
-    <img src="{{ asset('assets/customer/wave.svg') }}" alt="wave separator" class="w-full block relative z-10">
+
 
     {{-- =================== Info =================== --}}
-    <section class="bg-gradient-to-b from-[#e7ffea] to-blue-200 p-4 md:min-h-[1000px] relative -mt-20 z-0">
 
-        <h1 class="text-left p-10 text-4xl md:text-left md:text-7xl font-bold text-amber-500">Kepo Selecta!</h1>
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-6 p-10">
-            <!-- Card Besar (span 2 kolom, 2 baris) -->
-            <div
-                class="md:col-span-2 md:row-span-2 bg-[#e4ffbf] rounded-2xl shadow-lg flex flex-row md:flex-row overflow-hidden">
-                <div class="p-6 flex flex-col justify-between w-1/2 md:w-1/2">
-                    <h3 class="text-sm uppercase font-bold">SELECTA NEWS</h3>
-                    <p class="text-2xl font-bold mt-2">
-                        "Super fun! There's a lot of exciting activities you can choose!"
-                    </p>
+    {{-- <section class= "w-full relative h-[1000px]">
+        <div class="bg-blue-50 w-1/3 h-full text-center">Gallery Selecta</div>
 
-                    <button
-                        class="mt-6 px-4 py-2 bg-white rounded-lg text-black text-sm font-semibold hover:bg-gray-200 transition">
-                        MORE INFO >>
-                    </button>
-                </div>
-                <div class="w-1/2 md:w-1/2">
-                    <img src="{{ asset('assets/customer/peta-selecta1.jpg') }}"
-                        class="w-full h-full md:w-full md:h-full object-cover" />
-                </div>
-            </div>
 
-            <!-- Card Kecil 1 -->
-            <div class="bg-[#c0ffd7] rounded-2xl shadow-lg overflow-hidden">
-                <img src="{{ asset('assets/customer/peta-selecta1.jpg') }}" class="w-full h-40 object-cover" />
-                <div class="p-4">
-                    <h3 class="text-sm uppercase font-bold">FEEDING TIME!</h3>
-                    <p class="text-lg font-semibold mt-2">"The giraffe is so majestic and pretty"</p>
-                </div>
-            </div>
 
-            <!-- Card Kecil 2 -->
-            <div class="bg-[#edf0ac] rounded-2xl shadow-lg overflow-hidden">
-                <img src="{{ asset('assets/customer/peta-selecta1.jpg') }}" class="w-full h-40 object-cover" />
-                <div class="p-4">
-                    <h3 class="text-sm uppercase font-bold">ENCHANTING SHOWS</h3>
-                    <p class="text-lg font-semibold mt-2">"Amazing! A True Voyage of Fantasy"</p>
-                </div>
-            </div>
-        </div>
+    </section> --}}
 
-    </section>
-    <section class="bg-blue-200 p-4 md:min-h-[1000px]">
 
-        <h1 class="text-center p-10 text-4xl md:text-center md:text-7xl font-bold text-amber-500">Promo</h1>
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-6 p-10">
-            <!-- Card Besar (span 2 kolom, 2 baris) -->
-            <div
-                class="md:col-span-2 md:row-span-2 bg-[#e4ffbf] rounded-2xl shadow-lg flex flex-row md:flex-row overflow-hidden">
-                <div class="p-6 flex flex-col justify-between w-1/2 md:w-1/2">
-                    <h3 class="text-sm uppercase font-bold">SELECTA NEWS</h3>
-                    <p class="text-2xl font-bold mt-2">
-                        "Super fun! There's a lot of exciting activities you can choose!"
-                    </p>
-
-                    <button
-                        class="mt-6 px-4 py-2 bg-white rounded-lg text-black text-sm font-semibold hover:bg-gray-200 transition">
-                        MORE INFO >>
-                    </button>
-                </div>
-                <div class="w-1/2 md:w-1/2">
-                    <img src="{{ asset('assets/customer/peta-selecta1.jpg') }}"
-                        class="w-full h-full md:w-full md:h-full object-cover" />
-                </div>
-            </div>
-
-            <!-- Card Kecil 1 -->
-            <div class="bg-[#c0ffd7] rounded-2xl shadow-lg overflow-hidden">
-                <img src="{{ asset('assets/customer/peta-selecta1.jpg') }}" class="w-full h-40 object-cover" />
-                <div class="p-4">
-                    <h3 class="text-sm uppercase font-bold">FEEDING TIME!</h3>
-                    <p class="text-lg font-semibold mt-2">"The giraffe is so majestic and pretty"</p>
-                </div>
-            </div>
-
-            <!-- Card Kecil 2 -->
-            <div class="bg-[#edf0ac] rounded-2xl shadow-lg overflow-hidden">
-                <img src="{{ asset('assets/customer/peta-selecta1.jpg') }}" class="w-full h-40 object-cover" />
-                <div class="p-4">
-                    <h3 class="text-sm uppercase font-bold">ENCHANTING SHOWS</h3>
-                    <p class="text-lg font-semibold mt-2">"Amazing! A True Voyage of Fantasy"</p>
-                </div>
-            </div>
-        </div>
-
-    </section>
 
 
     {{-- =================== PETA SELECTA =================== --}}
+    <section class="">
+        <img src="{{ asset('assets/customer/peta2.jpg') }}" alt="" class="w-full">
 
+    </section>
+
+     <section class="bg-[#477fc1] p-4 md:min-h-[1000px] relative -mt-20 z-0">
+        <div class="flex justify-between items-center p-7">
+            <h1 class="text-left p-5 text-4xl md:text-left md:text-7xl font-bold text-[#ED3500] ">Kepo <span
+                    class="text-[#093FB4]">Selecta!</span></h1>
+            <a href=""
+                class="md:text-2xl text-center font-reguler text-[#093FB4] underline p-4 flex items-center gap-2">Selengkapnya
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="#093FB4">
+                    <g fill="none" stroke="#093FB4" stroke-width="1.5">
+                        <path stroke-linecap="round" stroke-miterlimit="10" d="m15.813 8.187l-7.626 7.626" />
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M16.111 15.155V8.917a1.028 1.028 0 0 0-1.028-1.028H8.845" />
+                        <rect width="18.5" height="18.5" x="2.75" y="2.75" rx="6" />
+                    </g>
+                </svg></a>
+
+
+
+        </div>
+
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-6 p-10">
+            <!-- Card Besar (span 2 kolom, 2 baris) -->
+            <div
+                class="md:col-span-2 md:row-span-2 bg-[#FFFCFB] rounded-2xl shadow-lg flex flex-row md:flex-row overflow-hidden">
+                <div class="p-6 flex flex-col justify-between w-1/2 md:w-1/2">
+                    <h3 class="text-sm uppercase font-bold">SELECTA NEWS</h3>
+                    <p class="text-2xl font-bold mt-2">
+                        "Super fun! There's a lot of exciting activities you can choose!"
+                    </p>
+
+                    <button
+                        class="mt-6 px-4 py-2 bg-[#093FB4] rounded-lg text-[#FFD8D8] text-sm font-semibold hover:bg-gray-200 transition">
+                        MORE INFO >>
+                    </button>
+                </div>
+                <div class="w-1/2 md:w-1/2">
+                    <img src="{{ asset('assets/customer/peta-selecta1.jpg') }}"
+                        class="w-full h-full md:w-full md:h-full object-cover" />
+                </div>
+            </div>
+
+            <!-- Card Kecil 1 -->
+            <div class="bg-[#FFFCFB] rounded-2xl shadow-lg overflow-hidden">
+                <img src="{{ asset('assets/customer/peta-selecta1.jpg') }}" class="w-full h-40 object-cover" />
+                <div class="p-4">
+                    <h3 class="text-sm uppercase font-bold">FEEDING TIME!</h3>
+                    <p class="text-lg font-semibold mt-2">"The giraffe is so majestic and pretty"</p>
+                </div>
+            </div>
+
+            <!-- Card Kecil 2 -->
+            <div class="bg-[#FFFCFB] rounded-2xl shadow-lg overflow-hidden">
+                <img src="{{ asset('assets/customer/peta-selecta1.jpg') }}" class="w-full h-40 object-cover" />
+                <div class="p-4">
+                    <h3 class="text-sm uppercase font-bold">ENCHANTING SHOWS</h3>
+                    <p class="text-lg font-semibold mt-2">"Amazing! A True Voyage of Fantasy"</p>
+                </div>
+            </div>
+        </div>
+
+
+
+    </section>
 
     {{-- =================== GOOGLE REVIEW =================== --}}
     {{-- <section class="bg-gray-50 py-20">
