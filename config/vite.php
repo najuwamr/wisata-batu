14b0ff22a3cd6cdd2e9014471c6c24ba73744390
KenRFH <riezqyken57@gmail.com>
2025-10-11 09:44:32 +0700

    index.php

---
+++ config/vite.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Path ke file manifest build Vite
+    |--------------------------------------------------------------------------
+    |
+    | Sesuaikan dengan lokasi folder "build" tempat kamu upload hasil
+    | "npm run build". Biasanya di hosting, folder ini ada di /public_html/build
+    |
+    */
+
+    'build_path' => 'public/build',
+
+];
